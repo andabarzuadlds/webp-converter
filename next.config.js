97b9f4d1903e7247e1e5a7f7d8d966c86bd176a9
andabarzuadlds <and.abarzua@dlds.cl>
2026-02-09 12:37:51 -0300

    fix
    another fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  images: {
+    unoptimized: true,
+  },
+  // Asegurar que el build no falle por problemas de tipos
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  eslint: {
+    ignoreDuringBuilds: false,
+  },
 };
 
 module.exports = nextConfig;
